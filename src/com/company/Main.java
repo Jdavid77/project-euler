9d02019f2e55457798822ca6c0943637a69b7b14
Jdavid77 <jdavid72009@hotmail.com>
2022-01-28 21:31:50 +0000

    P005

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,111 @@
 package com.company;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(findLargestPalindromeMadeFromProductOfNumber(3));
+        System.out.println(P005());
         //System.out.println(findLargestPalindromeMadeFromProductOfNumber);
 
     }
+
+    //Problem 1
+
+    public static long POO1(long number) {
+        long sum = 0;
+        for(long i = number - 1; i >= 3; i--){
+            if(i % 3 == 0 || i % 5 == 0){
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+
+    //Problem 2
+
+    public static int POO2(){
+
+        int reference = 4000000;
+        int evenValues = 2;
+
+        int x = 1;
+        int y = 2;
+
+        while (x <= 4000000){
+            if(x % 2 == 0){
+                evenValues += x;
+            }
+            int z = x + y;
+            x = y;
+            y = z;
+        }
+
+
+
+        return evenValues;
+
+
+    }
+
+
+    // Exercise 3
+
+    public static boolean isPrime(Long number){
+        boolean isItPrime = true;
+
+        if(number <= 1)
+        {
+            isItPrime = false;
+
+            return isItPrime;
+        }
+        else
+        {
+            for (int i = 2; i<= number/2; i++)
+            {
+                if ((number % i) == 0)
+                {
+                    isItPrime = false;
+
+                    break;
+                }
+            }
+
+            return isItPrime;
+        }
+    }
+
+    public static long P003(long number) {
+
+        long aux = number;
+        long i = 0;
+        long quociente = 0;
+        while(aux != 1){
+            if(isPrime(i)){
+                if(aux % i == 0){
+                    quociente = aux / i;
+                    aux = quociente;
+                }
+                else {
+                    i++;
+                }
+            }
+            else{
+                i++;
+            }
+        }
+
+        return i;
+
+    }
+
+
+
+    //
 
     public static long sumSquare (int number) {
 
@@ -26,6 +122,8 @@ public class Main {
 
     }
 
+    // Exercise 4
+
     public static boolean isPalindrome(long number){
 
         //fiz com string
@@ -37,7 +135,7 @@ public class Main {
         return false;
     }
 
-    public static long findLargestPalindromeMadeFromProductOfNumber (int number) {
+    public static long P004(int number) {
 
         long palindrome = 0;
 
@@ -54,6 +152,28 @@ public class Main {
             }
         }
         return palindrome;
+    }
+
+    //Exercise 5
+
+    public static int P005() {
+        boolean aux;
+
+
+        for(int i = 20; i <= 2147483647; i++){
+            aux = true;
+            for(int j = 1; j <= 20; j++){
+                if(i % j != 0){
+                    aux = false;
+                    break;
+                }
+            }
+            if (aux){
+                return i;
+            }
+        }
+
+        return -1;
     }
 
 
